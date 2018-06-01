c53656fe8092a9e7a03438d1693ad46b3636fd7b
Katlyn Tucker <studioquirk@yahoo.com>
2018-06-01 11:25:39 -0700

    Search page is rendering!

---
+++ src/components/Search.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './Search.css';
+
+export default class Search extends Component {
+
+  state = {
+    search: ''
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({ search:target.value});
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSearch(this.state);
+  };
+
+  render(){
+    const { search } = this.state;
+
+    return(
+      <form className="search-form" onSubmit={event => this.handleSubmit(event)}>
+        <label>
+          Search Categories: &nbsp;
+          <input value={search} onChange={this.handleChange}/>
+        </label>
+        <button>Search</button>
+      </form>
+    );
+  }
+}
