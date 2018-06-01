b6b47d1dcb89ef2e5e20029d922a564b9de73f36
Katlyn Tucker <studioquirk@yahoo.com>
2018-05-31 17:44:10 -0700

    modified script in html body

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.css';
-import App from './component/App';
-
+import App from './components/App';
 
 
 ReactDOM.render(
