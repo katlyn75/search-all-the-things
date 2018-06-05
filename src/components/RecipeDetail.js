6f37ac5aebeba449dd19b9fbd9bcec10703ec27c
Katlyn Tucker <studioquirk@yahoo.com>
2018-06-05 10:29:56 -0700

    wrote recipe detail component

---
+++ src/components/RecipeDetail.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class RecipeDetail extends Component {
+  render() {
+    return (
+      <div>
+        My cool recipe
+      </div>
+
+    )
+  }
+
+}
+
