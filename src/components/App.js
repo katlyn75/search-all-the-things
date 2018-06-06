b6b020abd1a272861a8fd23a99df538e3860a963
Katlyn Tucker <studioquirk@yahoo.com>
2018-06-05 17:31:48 -0700

    moving paging out of app into Paging

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,15 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-d
 import Header from './Header';
 import Home from './Home';
 import Search from './Search';
-//import Paging from './Paging';
+import Paging from './Paging';
 import RecipeDetail from './RecipeDetail';
 //import styles from './App.css';
 
 
 export default class App extends Component {
 
-  handlePage = ({ page }) => {
-    this.setState({ page }, this.searchRecipes);
-  };
-
   render() {
-    // const { loading, totalResults, page, perPage, error } = this.state;
+    
     return (
       <Router>
         <div>
@@ -24,17 +20,11 @@ export default class App extends Component {
             <Switch>
               <Route exact path="/" component={Home}/>
               <Route path="/search" component={Search}/>
+              <Route path="/paging" component={Paging}/>
               <Route path="/recipes/:id" render={({ match, history }) => {
                 return <RecipeDetail id={match.params.id} history={history}/>;}}/>
               <Redirect to="/"/>
-              {/*className= "page-results">
-              {loading && <div>Loading...</div>}
-              <Paging
-                totalResults={totalResults}
-                page={page}
-                perPage={perPage}
-                onPage={this.handlePage}/>
-              */}
+              
             </Switch>  
             <footer className="footer-text">
             Recipe Nirvana || 2018
--- src/components/Paging.js
+++ src/components/Paging.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
 
 export default class Paging extends Component {
+
+  handlePage = ({ page }) => {
+    this.setState({ page }, this.searchRecipes);
+  };
+
   handlePage(increment) {
     const { page, onPage } = this.props;
     onPage({ page: page + increment });
   }
 
   render() {
-    const { totalResults, page, perPage } = this.props;
+    
+    const {  page, /*totalResults, perPage, loading, error */} = this.props;
 
-    if(!totalResults) return <div>No results, try a different search</div>;
+    //if(!totalResults) return <div>No results, try a different search</div>;
 
-    const totalPages = Math.ceil(totalResults / perPage);
+    //const Results = Math.ceil(totalResults / perPage);
 
     return (
-      <div className="page">
-        <span className="page-numbers">Page {page} of {totalPages}</span>&nbsp;
-        <button className="page-button"onClick={() => this.handlePage(-1)} disabled={page === 1}>Prev&lt;</button>
-        <button className="page-button" onClick={() => this.handlePage(+1)} disabled={page === totalPages}>Next&gt;</button>
+      <div className="page-results">
+        <span className="page-numbers">Page {page} of { totalResults }</span>&nbsp;
+        <button className="page-button"
+          disabled={page === 1}
+          onClick={() => this.onPage(+page - 1)}>&lt;
+          Prev
+        </button>
+        {page}
+        <button className="page-button" onClick={() => this.onPage(+page + 1)}>&gt;
+          Next
+        </button>
       </div>
     );
   }
 }
+
+{/*className= "page-results">
+              {loading && <div>Loading...</div>}
+              <Paging
+                totalResults={totalResults}
+                page={page}
+                perPage={perPage}
+                onPage={this.handlePage}/>
+              */}
