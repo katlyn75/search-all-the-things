a09d4296f93a470508fea00fac5eb0729d712ef0
Katlyn Tucker <studioquirk@yahoo.com>
2018-06-05 17:31:18 -0700

    tweaked API data

--- src/services/recipeApi.js
+++ src/services/recipeApi.js
@@ -1,21 +1,21 @@
 const BASE_URL = 'https://www.themealdb.com/api/json/v1/1/';
+//https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772
 
 
-const throwJson = json => { throw json };
+const throwJson = json => { throw json; };
 const get = url => fetch(url)
-  .then(r => r.ok ? r.json(): r.json().then(throwJson));
+  .then(r => r.ok ? r.json() : r.json().then(throwJson));
 
 
 
-export function search({category},{ page=1, pageSize=5 }) {
+export function search({ category }, { page = 1, pageSize = 5 }) {
   const paging = `&page=${page}&pageSize=${pageSize}`;
   const search = `?c=${category}`;
 
-return get (`${BASE_URL}filter.php${search}`);
+  return get (`${BASE_URL}filter.php${search}`);
 }
 
 export function getRecipe(id) {
 
-return get (`${BASE_URL}lookup.php?i=${id}`)
+  return get (`${BASE_URL}lookup.php?i=${id}`);
 }
-//https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772
